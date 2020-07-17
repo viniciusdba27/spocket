27f6230bb1631544b995e5b212d0ecb305234868
Vinicius Abella <vinicius.abella@quest-global.com>
2020-07-16 21:09:15 -0700

    initializing

---
+++ App/Navigators/AppNavigator.js
@@ -0,0 +1,15 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+
+import ExampleScreen from 'App/Containers/Example/ExampleScreen'
+
+const StackNavigator = createStackNavigator(
+  {
+    MainScreen: ExampleScreen,
+  },
+  {
+    initialRouteName: 'MainScreen',
+    headerMode: 'none',
+  }
+)
+
+export default createAppContainer(StackNavigator)
---
+++ App/Stores/Example/Actions.js
@@ -0,0 +1,15 @@
+import { createActions } from 'reduxsauce'
+
+const { Types, Creators } = createActions({
+  // Fetch user informations
+  fetchUser: null,
+  // The operation has started and is loading
+  fetchUserLoading: null,
+  // User informations were successfully fetched
+  fetchUserSuccess: ['user'],
+  // An error occurred
+  fetchUserFailure: ['errorMessage'],
+})
+
+export const ExampleTypes = Types
+export default Creators
